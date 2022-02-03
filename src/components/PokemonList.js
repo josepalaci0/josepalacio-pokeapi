84080ba15874a2d49ad838966dc3150aada3ea2f
josepalaci0 <jose.palacio@esap.edu.co>
2022-02-02 19:13:26 -0500

    poke

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,50 +1,46 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import _, { divide, set } from "lodash";
-import { GetPokemonList, GetPokemonType } from "../actions/pokemonActions";
+import _ from "lodash";
 import { Link } from "react-router-dom";
-import ReactPaginate from "react-paginate";
 import PokemonInfo from "./PokemonInfo";
 import axios from "axios";
 
-
-
-
 const PokemonList = (props) => {
     const [pokemon, setPokemon] = useState([]);
+    const [pokemontype, setPokemontype] = useState([]);
     const [search, setSearch] = useState("");
     const [types, setTypes] = useState("");
-    const dispatch = useDispatch();
-    const pokemonList = useSelector(state => state.PokemonList);
-    const pokeData = pokemonList.data
-    React.useEffect(() => {
-       
-        FetchData(1)
+    const [condition, setCondition] = useState(true);
+
+
+    useEffect(() => {
         axios.get(`https://pokeapi.co/api/v2/type/`)
             .then(res => setTypes(res.data.results.map(el =>
-                (<option value={el.name} key={el.url}>{el.name}</option>))))
-    }, []);
+                (<option key={el.url} value={el.name} >{el.name}</option>))))
+
+        axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=6`).
+        then(res => setPokemon(res.data.results))
 
 
-    const FetchData = (page = 1) => {
-        dispatch(GetPokemonList(page))
+    }, []);    
+
+    const filterTypes = (name) => {
+        axios.get(`https://pokeapi.co/api/v2/type/${name}`)
+            .then(res => setPokemontype(res.data.pokemon));
+    };
+
+    const PokeTypesDefault = () =>{
+        axios.get(`https://pokeapi.co/api/v2/type/8`)
+            .then(res => setPokemontype(res.data.pokemon));
+        
     }
-   
-    console.log(pokeData)
-  
-   
 
-    const ShowData = () => {
-        if (pokemonList.loading) {
-            return <p>Loading...</p>
-        }
+     
 
-
-
-        if (!_.isEmpty(pokeData)) {
+    const ShowData1 = () => {
+        if (!_.isEmpty(pokemon)) {
             return (
                 <div className={"list-wrapper"}>
-                    {pokeData.map(el => (
+                    {pokemon.map(el => (
                         <div key={el.url} className={"pokemon-item"}>
 
 
@@ -59,59 +55,58 @@ const PokemonList = (props) => {
             )
         }
 
+    };
+
+    const ShowData2 = () => {
+        if (!_.isEmpty(pokemontype)) {
+            return (
+                <div className={"list-wrapper"}>
+                    {pokemontype.map(el => (
+                        <div key={el.pokemon.url} className={"pokemon-item"}>             
 
 
+                            <PokemonInfo url={el.pokemon.url} />
 
 
-        if (pokemonList.errorMsg !== "") {
-            return <p>{pokemonList.errorMsg}</p>
+                            <Link to={`/pokemon/${el.pokemon.name}`}><i className="fas fa-eye"></i></Link>
+                        </div>
+                    )
+                    )}
+                </div>
+            )
         }
 
-
-
-        return <p>unable to get data</p>
     };
-
-    const filterTypes = (name) => {
-        console.log('hola', name)
-
-        axios.get(`https://pokeapi.co/api/v2/type/${name}`)
-            .then(res => setPokemon(res.data.pokemon))
-    };
-    console.log(pokemon)
-
-
-
-
 
 
 
     return (
         <div>
-
-            <div className={"search-wrapper form"}>
+            <button onClick={() => setCondition(!condition)}>
+                Cambiar busqueda
+            </button>
+            {
+                condition
+                ?<div className={"search-wrapper form"}>
                 <p>Search: </p>
                 <input type="text" onChange={e => setSearch(e.target.value)} />
                 <button onClick={() => props.history.push(`/pokemon/${search}`)}>Search</button>
             </div>
-            <div className="select">
+                :<div className="select">
                 <h1>Types</h1>
                 <select onChange={e => filterTypes(e.target.value)}>
                     {types}
                 </select>
-
             </div>
-            {ShowData()}
+            }
+            {PokeTypesDefault()}
+            {
+                condition
+                ?ShowData1()
+                :ShowData2()
+            
+            }
 
-            {!_.isEmpty(pokemonList.data) && (
-                <ReactPaginate
-                    pageCount={Math.ceil(pokemonList.count / 15)}
-                    pageRangeDisplayed={2}
-                    marginPagesDisplayed={1}
-                    onPageChange={(data) => FetchData(data.selected + 1)}
-                    containerClassName={"pagination"}
-                />
-            )}
         </div>
     )
 };
